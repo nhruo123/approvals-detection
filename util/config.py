2e645bb74a2de5ccc4472871de07412b4e3cc924
Mayrom Rabinovich <nhruo123@gmail.com>
2024-02-10 14:18:51 +0200

    removed unused config

--- util/config.py
+++ util/config.py
@@ -2,6 +2,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings, frozen=True):
     infra_key: str
-    coin_gecko_key: str
 
     model_config = SettingsConfigDict(env_file=".env")
